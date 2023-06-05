76fac42408cc97d90aa658ba42b766fb5017c18a
Takeru Ohta <phjgt308@gmail.com>
2023-06-06 06:59:54 +0900

    Add sw.js

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,6 @@
-const CACHE_NAME = 'mineplacer-0.1.0';
+const CACHE_NAME = 'mineplacer';
+const CACHE_VERSION = '0.1.0';
+const CACHE_KEY = `${CACHE_NAME}-${CACHE_VERSION}`;
 
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
@@ -15,7 +17,7 @@ self.addEventListener('fetch', (e) => {
         caches.match(e.request).then((r) => {
             console.log('[Service Worker] Fetching resource: ' + e.request.url);
             return r || fetch(e.request).then((response) => {
-                return caches.open(CACHE_NAME).then((cache) => {
+                return caches.open(CACHE_KEY).then((cache) => {
                     console.log('[Service Worker] Caching new resource: ' + e.request.url);
                     cache.put(e.request, response.clone());
                     return response;
@@ -30,7 +32,7 @@ self.addEventListener('activate', (e) => {
     e.waitUntil(
         caches.keys().then((keyList) => {
             return Promise.all(keyList.map((key) => {
-                if(key !== CACHE_NAME) {
+                if(key.startsWith(CACHE_NAME) && key != CACHE_KEY) {
                     console.log(`[Service Worker] Delete old cache: ${key}`);
                     return caches.delete(key);
                 }
