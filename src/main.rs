0aa4314302028afdac5c82f76658f460a3709de5
Takeru Ohta <phjgt308@gmail.com>
2023-08-05 23:55:12 +0900

    Bump pagurus version to 0.7.1

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() -> pagurus::Result<()> {
     let mut game = mineplacer::game::Game::default();
     game.initialize(&mut system).or_fail()?;
     while let Ok(event) = system.next_event() {
-        if matches!(event, Event::Key(KeyEvent::Up { key: Key::Esc })) {
+        if matches!(event, Event::Key(KeyEvent { key: Key::Esc, .. })) {
             break;
         }
         if !game.handle_event(&mut system, event).or_fail()? {
