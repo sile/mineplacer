8f75d4670b356f784100d63cb7279f32e4b83e6b
Takeru Ohta <phjgt308@gmail.com>
2023-08-13 21:19:14 +0900

    Bump verion to 0.3.2

--- web/sw.js
+++ web/sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'mineplacer';
-const CACHE_VERSION = '0.3.1.3';
+const CACHE_VERSION = '0.3.2';
 const CACHE_KEY = `${CACHE_NAME}-${CACHE_VERSION}`;
 
 self.addEventListener('install', (e) => {
